bc83d123ffdfb9685ca6e301fcd70173b8603ea5
HarshTiwari-err <tiwari07.harsh@gmail.com>
2023-11-06 00:04:10 +0530

    updating app.js

--- server/app.js
+++ server/app.js
@@ -1,21 +1,23 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-const app = express()
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+// ! updating the documentation
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+import user from "./routes/user.js";
+import todo from "./routes/todo.js";
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-import user from './routes/user.js'
-import todo from './routes/todo.js'
+app.use("/user", user);
+app.use("/todo", todo);
 
-app.use('/user', user)
-app.use('/todo', todo)
-
-app.use(cors({
+app.use(
+  cors({
     origin: `${process.env.BASE_URL}`,
-}))
+  })
+);
 
-export default app
+export default app;
